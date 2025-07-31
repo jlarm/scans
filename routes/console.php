d9f5f532dfd5fb5b2a16635ed0b32dde82d2bb82
jlarm <117611704+jlarm@users.noreply.github.com>
2025-07-31 14:56:40 -0500

    Add scheduled command to run scan process every second

--- routes/console.php
+++ routes/console.php
@@ -5,6 +5,4 @@ declare(strict_types=1);
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Schedule::command('scans:process')->everySecond();
